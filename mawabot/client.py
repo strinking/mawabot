9bc5113d2b1bf6bd8e8f5aebd8fad43ea2602ee4
Ammon Smith <ammon.i.smith@gmail.com>
2017-07-06 12:57:07 -0700

    Add license headers.

--- mawabot/client.py
+++ mawabot/client.py
@@ -1,7 +1,20 @@
+#
+# client.py
+#
+# mawabot - Maware's selfbot
+# Copyright (c) 2017 Ma-wa-re, Ammon Smith
+#
+# mawabot is available free of charge under the terms of the MIT
+# License. You are free to redistribute and/or modify it under those
+# terms. It is distributed in the hopes that it will be useful, but
+# WITHOUT ANY WARRANTY. See the LICENSE file for more details.
+#
+
 '''
 client.py
 Holds the custom discord client
 '''
+
 import datetime
 
 import discord
--- mawabot/cogs/general.py
+++ mawabot/cogs/general.py
@@ -1,4 +1,17 @@
+#
+# cogs/general.py
+#
+# mawabot - Maware's selfbot
+# Copyright (c) 2017 Ma-wa-re, Ammon Smith
+#
+# mawabot is available free of charge under the terms of the MIT
+# License. You are free to redistribute and/or modify it under those
+# terms. It is distributed in the hopes that it will be useful, but
+# WITHOUT ANY WARRANTY. See the LICENSE file for more details.
+#
+
 ''' Holds general commands for self bot '''
+
 from random import randint
 
 import discord
