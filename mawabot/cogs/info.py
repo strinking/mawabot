73f16b346ebfe9bfeaf1c5d3ae448aeb66b2d74a
Ammon Smith <ammon.i.smith@gmail.com>
2017-07-06 17:12:24 -0700

    Add .uinfo

--- mawabot/cogs/info.py
+++ mawabot/cogs/info.py
@@ -31,7 +31,100 @@ class Info:
     def __init__(self, bot):
         self.bot = bot
 
+    # Helper methods
+    def _get_users(self, names):
+        if not names:
+            return [self.bot.user]
+
+        users = []
+        for name in names:
+            try:
+                id = int(name)
+                user = self.bot.get_user(id)
+                if user:
+                    users.append(user)
+            except ValueError:
+                for user in self.bot.users:
+                    if user.name == name:
+                        users.append(user)
+                        break
+        return users
+
+    def _get_members(self, guild, names):
+        if not names:
+            return [guild.me]
+
+        members = []
+        for name in names:
+            try:
+                id = int(name)
+                member = guild.get_member(id)
+                if member:
+                    members.append(member)
+            except ValueError:
+                for member in guild.members:
+                    if member.name == name or member.nick == name:
+                        members.append(member)
+                        break
+        return members
+
     # Commands
+    @commands.command()
+    async def uinfo(self, ctx, *names: str):
+        ''' Gets information about the given user(s) '''
+
+        if ctx.guild is None:
+            users = self._get_users(names)
+        else:
+            users = self._get_members(ctx.guild, names)
+
+        for user in users:
+            lines = [
+                f'{user.mention} - `{user.id}`',
+            ]
+
+            if isinstance(user, discord.Member):
+                lines.append(f'Status: {user.status}')
+
+                if user.game:
+                    if user.game.type == 1:
+                        lines.append(f'Streaming {user.game.url}')
+                    else:
+                        lines.append(f'Playing {user.game.name}')
+
+                if user.voice:
+                    mute = user.voice.mute or user.voice.self_mute
+                    deaf = user.voice.deaf or user.voice.self_deaf
+
+                    states = []
+                    if mute:
+                        states.append('muted')
+                    if deaf:
+                        states.append('deafened')
+
+                    if states:
+                        state = ', '.join(states)
+                    else:
+                        state = 'active'
+
+                    lines.append(f'Voice: {state}')
+
+                if user.nick:
+                    lines.append(f'Nickname: {user.nick}')
+
+                roles = ' '.join(map(lambda x: x.mention, user.roles[1:]))
+                if roles:
+                    lines.append(f'Roles: {roles}')
+
+            embed = discord.Embed(type='rich', description='\n'.join(lines))
+            if hasattr(user, 'color'):
+                embed.color = user.color
+
+            name = f'{user.name}#{user.discriminator}'
+            embed.set_author(name=name, icon_url=user.avatar_url)
+            embed.timestamp = user.created_at
+            await ctx.send(embed=embed)
+
     @commands.command()
     async def emoji(self, ctx, *emojis: str):
         ''' Gets information about the given emoji(s) '''
