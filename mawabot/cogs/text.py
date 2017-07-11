a020d3569f31a98690bd1d9c7c5dd307ad26eb59
Ma-wa-re <Ma-wa-re@users.noreply.github.com>
2017-07-11 11:12:12 +0100

    Added rev and kerrhau command
    Added the rev command that reverses the text given.
    
    Added the kerrhau command that "kerrhau"-ifys the given text for meme emphasis

--- mawabot/cogs/text.py
+++ mawabot/cogs/text.py
@@ -12,6 +12,7 @@
 
 ''' Has commands for text transformation '''
 import codecs
+from random import randint
 
 import discord
 from discord.ext import commands
@@ -81,6 +82,38 @@ class Text:
         lines = [text] + list(text[1:])
 
         await ctx.message.edit(content='\n'.join(lines))
+
+    @commands.command()
+    async def rev(self, ctx, *, text: str):
+        ''' Reverses the text given '''
+
+        await ctx.message.edit(content=text[::-1])
+
+    @commands.command()
+    async def kerrhau(self, ctx, *text: str):
+        ''' "kerrhau"-ifys the given text for meme emphasis '''
+
+        text = list(text)
+        words = []
+
+        while text:
+            rand_num = randint(1, 3)
+            word = []
+
+            try:
+                for _ in range(rand_num):
+                    word.append(text.pop(0))
+            except:
+                pass
+
+            words.append(' '.join(word))
+
+        last = words[-1][-1]
+        words[-1] = words[-1][:-1]
+        words.append(last)
+
+        await ctx.message.edit(content='\n'.join(words))
+
 
 def setup(bot):
     ''' Setup function to add cog to bot '''
