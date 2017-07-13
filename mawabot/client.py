4b17c7bb74e17df42970f399705f30863fed8135
Ma-wa-re <Ma-wa-re@users.noreply.github.com>
2017-07-13 14:15:36 +0100

    Made bot change status back to invis on reconnect

--- mawabot/client.py
+++ mawabot/client.py
@@ -104,6 +104,12 @@ class Bot(commands.Bot):
 
         await self.change_presence(status=discord.Status.invisible)
         logger.info('Setting status to invisible')
+    
+    async def on_resumed(self):
+        ''' Used when the bot reconnects '''
+
+        logger.info('Reconnected - setting status to invisible')
+        await self.change_presence(status=discord.Status.invisible)
 
     async def _send(self, *args, **kwargs):
         if self.output_chan is None:
