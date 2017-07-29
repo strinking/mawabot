6e422c7bfafdb3014e9422565fbcefc467cc7805
Ammon Smith <ammon.i.smith@gmail.com>
2017-07-28 23:24:50 -0700

    Add .ri

--- mawabot/cogs/text/meme.py
+++ mawabot/cogs/text/meme.py
@@ -13,6 +13,7 @@
 ''' Has commands for meme-y text transformation '''
 
 import random
+import re
 
 from discord.ext import commands
 
@@ -20,28 +21,79 @@ __all__ = [
     'Meme',
 ]
 
+DISCORD_STRINGS = re.compile(r'(<\S*>)')
+
 class Meme:
     __slots__ = (
         'bot',
+        'regional_emojis',
     )
 
     def __init__(self, bot):
         self.bot = bot
+        self.regional_emojis = {
+            'a': '\N{REGIONAL INDICATOR SYMBOL LETTER A}',
+            'b': '\N{REGIONAL INDICATOR SYMBOL LETTER B}',
+            'c': '\N{REGIONAL INDICATOR SYMBOL LETTER C}',
+            'd': '\N{REGIONAL INDICATOR SYMBOL LETTER D}',
+            'e': '\N{REGIONAL INDICATOR SYMBOL LETTER E}',
+            'f': '\N{REGIONAL INDICATOR SYMBOL LETTER F}',
+            'g': '\N{REGIONAL INDICATOR SYMBOL LETTER G}',
+            'h': '\N{REGIONAL INDICATOR SYMBOL LETTER H}',
+            'i': '\N{REGIONAL INDICATOR SYMBOL LETTER I}',
+            'j': '\N{REGIONAL INDICATOR SYMBOL LETTER J}',
+            'k': '\N{REGIONAL INDICATOR SYMBOL LETTER K}',
+            'l': '\N{REGIONAL INDICATOR SYMBOL LETTER L}',
+            'm': '\N{REGIONAL INDICATOR SYMBOL LETTER M}',
+            'n': '\N{REGIONAL INDICATOR SYMBOL LETTER N}',
+            'o': '\N{REGIONAL INDICATOR SYMBOL LETTER O}',
+            'p': '\N{REGIONAL INDICATOR SYMBOL LETTER P}',
+            'q': '\N{REGIONAL INDICATOR SYMBOL LETTER Q}',
+            'r': '\N{REGIONAL INDICATOR SYMBOL LETTER R}',
+            's': '\N{REGIONAL INDICATOR SYMBOL LETTER S}',
+            't': '\N{REGIONAL INDICATOR SYMBOL LETTER T}',
+            'u': '\N{REGIONAL INDICATOR SYMBOL LETTER U}',
+            'v': '\N{REGIONAL INDICATOR SYMBOL LETTER V}',
+            'w': '\N{REGIONAL INDICATOR SYMBOL LETTER W}',
+            'x': '\N{REGIONAL INDICATOR SYMBOL LETTER X}',
+            'y': '\N{REGIONAL INDICATOR SYMBOL LETTER Y}',
+            'z': '\N{REGIONAL INDICATOR SYMBOL LETTER Z}',
+            '0': '0\N{COMBINING ENCLOSING KEYCAP}',
+            '1': '1\N{COMBINING ENCLOSING KEYCAP}',
+            '2': '2\N{COMBINING ENCLOSING KEYCAP}',
+            '3': '3\N{COMBINING ENCLOSING KEYCAP}',
+            '4': '4\N{COMBINING ENCLOSING KEYCAP}',
+            '5': '5\N{COMBINING ENCLOSING KEYCAP}',
+            '6': '6\N{COMBINING ENCLOSING KEYCAP}',
+            '7': '7\N{COMBINING ENCLOSING KEYCAP}',
+            '8': '8\N{COMBINING ENCLOSING KEYCAP}',
+            '9': '9\N{COMBINING ENCLOSING KEYCAP}',
+            '!': '\N{HEAVY EXCLAMATION MARK SYMBOL}',
+            '?': '\N{BLACK QUESTION MARK ORNAMENT}',
+        }
+
+    @commands.command()
+    async def ri(self, ctx, *, text: str):
+        ''' Makes the whole message into regional_indicator emojis '''
+
+        def mapper(s):
+            if s.startswith('<'):
+                return s
+            return '\u200b'.join(self.regional_emojis.get(c.lower(), c) for c in s)
+
+        content = ''.join(map(mapper, DISCORD_STRINGS.split(text)))
+        await ctx.message.edit(content=content)
 
     @commands.command()
     async def sw(self, ctx, *, text: str):
-        ''' Spaces out words for meme emphasis '''
+        ''' Spaces out words '''
 
-        words = text.split(' ')
-        result = []
-        for word in words:
-            result.append(' '.join(word))
-
-        await ctx.message.edit(content=' . '.join(result))
+        content = ' . '.join(' '.join(word) for word in text.split(' '))
+        await ctx.message.edit(content=content)
 
     @commands.command()
     async def cw(self, ctx, *, text: str):
-        ''' "Crossword"-ifys the given text for meme emphasis '''
+        ''' "Crossword"-ifys the given text '''
 
         text = text.upper()
         lines = [text] + list(text[1:])
@@ -50,7 +102,7 @@ class Meme:
 
     @commands.command()
     async def kerrhau(self, ctx, *text: str):
-        ''' "kerrhau"-ifys the given text for meme emphasis '''
+        ''' "kerrhau"-ifys the given text '''
 
         text = list(text)
         words = []
